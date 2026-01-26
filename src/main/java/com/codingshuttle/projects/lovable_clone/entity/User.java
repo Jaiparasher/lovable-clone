cc3291624e614d8178afe3b460cb194afcf3cf87
jaiparasher <jaiparasher30@gmail.com>
2026-01-26 14:54:17 +0530

    Initial commit

---
+++ src/main/java/com/codingshuttle/projects/lovable_clone/entity/User.java
@@ -0,0 +1,31 @@
+package com.codingshuttle.projects.lovable_clone.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String email;
+    String passwordHash;
+    String name;
+
+    String avatarUrl;
+    Instant createdAt;
+    Instant updatedAt;
+
+    Instant deletedAt; //soft delete
+}
