03d1e36e07b641f122416d0ba78f8439f1c73464
Jai Parasher <jaiparasher30@gmail.com>
2026-04-14 17:39:04 +0530

    AI Setup and configuration

--- src/main/java/com/codingshuttle/projects/lovable_clone/service/impl/AiGenerationServiceImpl.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/service/impl/AiGenerationServiceImpl.java
@@ -1,11 +1,18 @@
 package com.codingshuttle.projects.lovable_clone.service.impl;
 
 import com.codingshuttle.projects.lovable_clone.service.AiGenerationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class AiGenerationServiceImpl implements AiGenerationService {
+
+    private final ChatClient chatClient;
 
     @Override
     public Flux<String> streamResponse(String message, Long aLong) {
